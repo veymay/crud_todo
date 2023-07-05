088ac3d2bedaabbf7a9be4d54bec58b3bc78995c
VEYMAY <veymayuz@gmail.com>
2023-07-05 22:30:56 +0500

    adding fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
+'use strict'
+
 let todoAddInput = document.querySelector('.todo_add>input'),
-  todoAddButton = document.querySelector('.todo_add>button'),
-  todoList = document.querySelector('.todo_list'),
-  todoItem = document.querySelectorAll('.todo_list>li'),
-  todoItemCheckbox = document.querySelectorAll('.todo_list>li>input')
+todoAddButton = document.querySelector('.todo_add>button'),
+todoList = document.querySelector('.todo_list'),
+todoItem = document.querySelectorAll('.todo_list>li'),
+todoItemCheckbox = document.querySelectorAll('.todo_list>li>input')
 
 document.addEventListener('keydown', () => {
   todoAddInput.focus()
+})
+document.addEventListener('keydown', (e) => {
+  todoAddInput.focus()
+  if (e.key == "Enter") {
+    if (todoAddInput.value.length>2) {
+      todoList.innerHTML += `<li>${todoAddInput.value}</li>`
+      todoAddInput.value = ''
+    }
+  }
 })
 
 todoAddButton.addEventListener('click', () => {
@@ -17,7 +28,7 @@ todoAddButton.addEventListener('click', () => {
 
 todoItem.forEach(item => {
   item.addEventListener('click', (e) => {
-    item.classList.toggle('active')
     e.stopPropagation()
+    e.classList.toggle('active')
   })
 });
