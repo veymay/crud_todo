b647f116007c1a00aaf29dcd207a8c906332bc96
VEYMAY <veymayuz@gmail.com>
2023-07-30 20:05:23 +0500

    newww

--- js/main.js
+++ js/main.js
@@ -1,42 +1,12 @@
 let input = document.querySelector('.todo_input'),
   list = document.querySelector('.todo_list'),
-  listItem = document.querySelectorAll('.todo_list>li'),
+  remove = document.querySelectorAll('.remove'),
   todos = JSON.parse(localStorage.getItem('todos'))?JSON.parse(localStorage.getItem('todos')):[] 
-// document.addEventListener('click', () => {
-//   input.focus()
-// })
 
-getTodo(todos)
-// Set TODO
-function setTodo(inp,arr) {
-  if (inp.value.length > 3) {
-    inp.value.trim()
-    arr.push(inp.value)
-    inp.value = ''
-    localStorage.setItem('todos',JSON.stringify(todos))
-    console.log(arr);
-  }
-}
-// Get TODO
-function getTodo(todoArr) {
-  todoArr.forEach(todoItem => {
-    list.innerHTML += `<li class='items'>${todoItem}<div class="buttons"><span class="edit"><i class="fi fi-br-pencil"></i></span> <span class="remove"><i class="fi fi-br-cross"></i></span> </div></li>`
-  });
-}
-
-// Complete TODO
-listItem.forEach(element => {
-  element.addEventListener('click', () => {
-    alert('s')
-  })
-});
-// Remove TODO
-function removeTodo(todoArr) {
-  todoArr.forEach(todoItem => {
-    todoItem.remove
-  });
-}
-// /////////////////////////////////////////////
+window.addEventListener('click', (e) => {
+  e.stopPropagation()
+})
+///////////////////////////////////////////////
 document.addEventListener('keydown', (e) => {
   input.focus()
   if (e.key == 'Enter') {
@@ -46,5 +16,34 @@ document.addEventListener('keydown', (e) => {
   } 
 })
 
-
-// list.innerHTML+=`<li class='item'>${input.value}</li>`
+// Set TODO
+function setTodo(inp,arr) {
+  if (inp.value.length > 0) {
+    inp.value.trim()
+    arr.push({text: inp.value, completed: false})
+    inp.value = ''
+    localStorage.setItem('todos',JSON.stringify(todos))
+  }
+}
+// Get TODO
+function getTodo(todoArr) {
+  todoArr.forEach((todos,index) => {
+    list.innerHTML += `<li class='item ${todos.completed?'completed':''}' onclick="completedTODO(${index})"><div class="content">${todos.text}</div><div class="buttons"><span class="edit"> <span class="remove" onclick="event.stopPropagation();deleteTODO(${index})"><i class="fi fi-br-cross"></i></span> </div></li>`
+  });
+}
+// Completed Todo
+function completedTODO(index) {
+  listItem = document.querySelectorAll('li.item');
+  listItem[index].classList.toggle('completed');
+  let get = JSON.parse(localStorage.getItem("todos"));
+  if (listItem[index].classList.contains("completed")) {
+    get[index]["completed"] = true;
+  } else get[index]["completed"] = false;
+  localStorage.setItem("todos", JSON.stringify(get));
+};
+// Delete TODO
+function deleteTODO(index) {
+  listItem = document.querySelectorAll('li.item');
+  listItem[index].remove()
+};
+getTodo(todos)
