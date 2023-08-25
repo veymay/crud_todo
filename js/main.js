d26bf24667035551226e15bbbcdb4ce71b6d8573
VEYMAY <veymayuz@gmail.com>
2023-08-26 03:07:59 +0500

    add crud

--- js/main.js
+++ js/main.js
@@ -1,40 +1,85 @@
 let todoInp = document.querySelector('.todo_input')
 let todoList = document.querySelector('ul.todo_list')
-
-
-let todos = JSON.parse(localStorage.getItem('todosLIST'))?JSON.parse(localStorage.getItem('todosLIST')):[]
-
-function delTODO(index,id) {
-  if (index >= 0) {
-    todos.splice(index, 1);
-  }
-}
+let numberAll = document.querySelector('.number>.all')
+let todoListItems = document.querySelectorAll('ul.todo_list>.item')
+let todos = JSON.parse(localStorage.getItem('todosLIST')) ? JSON.parse(localStorage.getItem('todosLIST')) : []
+setInterval(() => {
+}, 1000);
 function setTODO() {
-  todos.push(todoInp.value)
-  todoInp.value = ''
   localStorage.setItem('todosLIST', JSON.stringify(todos))
 }
-function getTODO() {
-  todoList.innerHTML = ''
-  todos.forEach((todo,id) => {
-    todoList.innerHTML += `
-      <li class='item'>
-        <div class="content">${todo}</div>
-        <div class="buttons">
-          <span class="remove" onclick="${delTODO(id)}">
-            <i class="fi fi-br-cross"></i>
-          </span>
-        </div>
-      </li>`
-  });
-}
 
+//* Add TODO
 document.addEventListener('keydown', (e) => {
-  todoInp.focus()
   if (e.key == 'Enter') {
-    setTODO()
-    getTODO()
+    if (todoInp.value != '') {
+      todos.push({
+        text: todoInp.value,
+        completed: ''
+      })
+      setTODO()
+      todoInp.value = ''
+      showTODO()
+    }
   }
 })
+//* Show TODO
+function showTODO() {
+  todoList.innerHTML = ''
+  numberAll.innerHTML = `<i class="fi fi-br-dot-circle"></i>${todos.length}`
+  todos.forEach((todo, id) => {
+    todoList.innerHTML += `
+    <li class='item ${todo.completed}'>
+    <div class="content">
+    <input type="text" value="${todo.text}" readonly>
+    </div>
+    <div class="buttons">
+    <span class="edit" onclick="editTODO(${id})">
+    <i class="fi fi-br-pencil"></i>
+    </span>
+    <span class="remove" onclick="delTODO(${id})">
+    <i class="fi fi-br-cross"></i>
+    </span>
+    </div >
+    </li > `
+    todoListItems = document.querySelectorAll('ul.todo_list>.item')
+  });
+}
+showTODO()
+//? Completed TODO
+todoListItems.forEach((item, id) => {
+  item.addEventListener('click', () => {
+    console.log('salom');
+    todos[id].completed == '' ? todos[id].completed = 'completed' : todos[id].completed = ''
+    setTODO()
+  })
+})
+todoListItems.forEach((item, id) => {
+  item.addEventListener('click', () => {
+    showTODO()
+  })
+})
 
-getTODO()
+//? Edit TODO
+function editTODO(id) {
+  let buttons = document.querySelectorAll('.buttons')
+  let editBtn = document.querySelectorAll('.buttons>span.edit')
+  let todoItemInp = document.querySelectorAll('.content>input')
+  if (buttons[id].classList.contains('active')) {
+    editBtn[id].innerHTML = '<i class="fi fi-br-pencil"></i>'
+  } else {
+    editBtn[id].innerHTML = '<i class="fi fi-br-check"></i>'
+  }
+  buttons[id].classList.toggle('active')
+  todoItemInp[id].classList.toggle('active')
+  todoItemInp[id].toggleAttribute('readonly')
+  todos[id].text = todoItemInp[id].value
+  setTODO()
+}
+
+//! Delete TODO
+function delTODO(id) {
+  todos.splice(id, 1);
+  showTODO()
+  setTODO()
+}
